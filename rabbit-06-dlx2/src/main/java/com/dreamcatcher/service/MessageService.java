bbd77deeaa69f168258f56060135a3f5b52f1267
zgl <1654017271@qq.com>
2023-05-29 21:46:17 +0800

    修改了主机

--- rabbit-06-dlx2/src/main/java/com/dreamcatcher/service/MessageService.java
+++ rabbit-06-dlx2/src/main/java/com/dreamcatcher/service/MessageService.java
@@ -23,7 +23,9 @@ public class MessageService {
         MessageProperties messageProperties = new MessageProperties();
         //设置单条消息过期时间 数据类型是字符串
         messageProperties.setExpiration("15000");  // 设置消息过期时间 15s
-        Message message = MessageBuilder.withBody("Hello world!".getBytes()).build();
+        Message message = MessageBuilder.withBody("Hello world!".getBytes())
+                .andProperties(messageProperties)
+                .build();
         // 发送消息  绑定正常交换机和正常队列
         rabbitTemplate.convertAndSend("exchange.normal.2", "order", message);
         log.info("消息发送成功,发送时间:{}", new Date());
