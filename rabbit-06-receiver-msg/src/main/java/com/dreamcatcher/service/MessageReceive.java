b26a67df5fc8af2e53fa6300380d9ecbd99a95d8
Augustus Aherns <zlucky1654017@gmail.com>
2023-05-31 22:56:01 +0800

    day3

--- rabbit-06-receiver-msg/src/main/java/com/dreamcatcher/service/MessageReceive.java
+++ rabbit-06-receiver-msg/src/main/java/com/dreamcatcher/service/MessageReceive.java
@@ -13,33 +13,34 @@ import java.util.Date;
 
 @Component
 @Slf4j
-public class MessageReceive{
-   @RabbitListener(queues = {"queue.normal.4"})
-    public void receiveMsg(Message message, Channel channel){
-      //获取消息属性
-      MessageProperties properties = message.getMessageProperties();
-      properties.getDeliveryTag(); //获取消息的tagId 用于手动确认消息 唯一标识（身份证号）
-      //获取消息的唯一标识，类似身份证或学号
+public class MessageReceive {
+    //Channel channel(信道) 用于手动确认消息
+    @RabbitListener(queues = {"queue.normal.4"})
+    public void receiveMsg(Message message, Channel channel) {
+        //获取消息属性
+        MessageProperties properties = message.getMessageProperties();
 
+        //获取消息的唯一标识，类似身份证或学号
+        long deliveryTag = properties.getDeliveryTag();//获取消息的tagId 用于手动确认消息 唯一标识（身份证号）
 
-      try {
-         byte[] body = message.getBody();
-         String msg = new String(body);
-         log.info("receive msg:{},接收时间为:{}",msg,new Date());
-         //TODO 业务处理
-         int i = 1/0;
-         //手动确认消息 参数1：消息的tagId 参数2：是否批量确认
-         channel.basicAck(properties.getDeliveryTag(),false);
+        try {
+            byte[] body = message.getBody();
+            String msg = new String(body);
+            log.info("receive msg:{},接收时间为:{}", msg, new Date());
+            //TODO 业务处理
+            int i = 1 / 0;
+            //手动确认消息 参数1：消息的tagId 参数2：是否批量确认
+            channel.basicAck(deliveryTag, false);
 
-      } catch (Exception e) {
-         log.error("接收者出现问题:{}",e.getMessage());
-         try {
-            //拒绝消息 参数1：消息的tagId 参数2：是否批量拒绝 参数3：是否重回队列
-            channel.basicNack(properties.getDeliveryTag(),false,false);
-         } catch (IOException ex) {
-            throw new RuntimeException(ex);
-         }
-         throw new RuntimeException(e);
-      }
-   }
+        } catch (Exception e) {
+            log.error("接收者出现问题:{}", e.getMessage());
+            try {
+                //拒绝消息 参数1：消息的tagId 参数2：是否批量拒绝 参数3：是否重回队列
+                channel.basicNack(properties.getDeliveryTag(), false, false);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ rabbit-07-delay2-problem1/src/main/java/com/dreamcatcher/service/MessageService.java
@@ -0,0 +1,47 @@
+package com.dreamcatcher.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@Service
+@Slf4j
+public class MessageService {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${my.exchangeName}")
+    private String exchangeName;
+
+    public void send() {
+        {
+            MessageProperties messageProperties = new MessageProperties();
+            messageProperties.setExpiration("25000");    // 设置消息的过期时间 15s 毫秒数
+            Message message = MessageBuilder.withBody("Hello world!".getBytes())
+                    .andProperties(messageProperties)
+                    .build();
+            // 发送消息 头部交换机没有路由键
+            rabbitTemplate.convertAndSend(exchangeName, "order", message);
+            log.info("消息发送成功,发送时间:{}", new Date());
+        }
+        {
+            MessageProperties messageProperties = new MessageProperties();
+            messageProperties.setExpiration("25000");    // 设置消息的过期时间 15s 毫秒数
+            Message message = MessageBuilder.withBody("Hello world!".getBytes())
+                    .andProperties(messageProperties)
+                    .build();
+            // 发送消息 头部交换机没有路由键
+            rabbitTemplate.convertAndSend(exchangeName, "order", message);
+            log.info("消息发送成功,发送时间:{}", new Date());
+        }
+    }
+}
